06dc770f097acab212ae935bcc0a4ef39aaa5619
SolisSaturnus <77696391+SolisSaturnus@users.noreply.github.com>
2021-02-12 23:41:48 -0600

    Add Roll-A-Ball

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+
+ void OnTriggerEnter(Collider other)   
+    {
+        SceneManager.LoadScene(index);
+
+        //SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Assets/Scripts/RestartTrigger.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartTrigger : MonoBehaviour
+{
+    private Scene scene;
+   void Start()
+    {
+        scene = SceneManager.GetActiveScene(); 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(scene.name);
+        }
+        else
+        {
+
+        }
+    }
+}
